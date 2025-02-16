471eeff91d0f7fc697b455526697b6a0f26f55a8
icchi-saya <71650432+icchi-saya@users.noreply.github.com>
2025-02-16 18:40:58 +0900

    Initial commit

---
+++ App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Counter />
+  </div>
+</template>
+
+<script>
+import Counter from './Counter.vue';
+
+export default {
+  components: {
+    Counter
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
---
+++ Counter.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>カウント: {{ count }}</h1>
+    <button @click="increment">カウントアップ</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    }
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+button {
+  background-color: #42b983;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+button:hover {
+  background-color: #367a6e;
+}
+</style>
